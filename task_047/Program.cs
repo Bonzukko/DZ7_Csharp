5bf6f818ffa60c67fc1a81c308a9414d518dcb5f
Dmitry Fedotov <fedotov-ds1989@yandex.ru>
2022-09-17 11:08:35 +0300

    task_047

---
+++ task_047/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+int m = new Random().Next(2, 11);
+int n = new Random().Next(2, 11);
+Console.WriteLine($"m = {m}, n = {n}.");
+double[,] matrix = new double[m, n];
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        matrix[i, j] = new Random().Next(-99, 100) + Math.Round(new Random().NextDouble(), 2);
+        Console.Write(matrix[i, j] + "\t");
+    }
+    Console.WriteLine();
+}
